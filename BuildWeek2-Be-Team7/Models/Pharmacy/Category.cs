56dc13d58335a05521e6c7b229f61c56e3c55f65
Domy15 <domenico2016dellaventura@gmail.com>
2025-03-31 12:00:30 +0200

    modelli prodotto

---
+++ BuildWeek2-Be-Team7/Models/Pharmacy/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek2_Be_Team7.Models.Pharmacy
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ BuildWeek2-Be-Team7/Models/Pharmacy/Company.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek2_Be_Team7.Models.Pharmacy
+{
+    public class Company
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Address { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public required string Tel { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
