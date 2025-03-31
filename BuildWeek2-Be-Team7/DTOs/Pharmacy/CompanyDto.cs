72fab48aaeff51d373540b299ddd9bb8a0062852
Domy15 <domenico2016dellaventura@gmail.com>
2025-03-31 14:48:40 +0200

    commit

---
+++ BuildWeek2-Be-Team7/DTOs/Pharmacy/CompanyDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuildWeek2_Be_Team7.DTOs.Pharmacy
+{
+    public class CompanyDto
+    {
+        public required string Name { get; set; }
+
+        public required string Address { get; set; }
+
+        public required string Tel { get; set; }
+    }
+}
