8edbb39d31bf2017efe00c97ed81c32638556cd0
Domy15 <domenico2016dellaventura@gmail.com>
2025-04-01 15:29:06 +0200

    commit

--- BuildWeek2-Be-Team7/Services/HospitalizationServices.cs
+++ BuildWeek2-Be-Team7/Services/HospitalizationServices.cs
@@ -1,5 +1,6 @@
 ﻿using BuildWeek2_Be_Team7.Data;
 using BuildWeek2_Be_Team7.DTOs.Hospitalization;
+using BuildWeek2_Be_Team7.DTOs.Owner;
 using BuildWeek2_Be_Team7.DTOs.Pet;
 using BuildWeek2_Be_Team7.Models.Animali;
 using Microsoft.AspNetCore.Authorization;
@@ -11,7 +12,7 @@ namespace BuildWeek2_Be_Team7.Services
     public class HospitalizationServices
     {
         private readonly ApplicationDbContext _context;
-        public HospitalizationServices (ApplicationDbContext context)
+        public HospitalizationServices(ApplicationDbContext context)
         {
             _context = context;
         }
@@ -29,7 +30,7 @@ namespace BuildWeek2_Be_Team7.Services
 
         public async Task<bool> AddNewHospit(AddHospitalizationDto addHospitalizationDto)
         {
-            var pet = await _context.Pets.FirstOrDefaultAsync(p=> p.PetId == addHospitalizationDto.PetId);
+            var pet = await _context.Pets.FirstOrDefaultAsync(p => p.PetId == addHospitalizationDto.PetId);
             if (pet == null) { return false; }
             var newHospit = new Hospitalization()
             {
@@ -43,8 +44,8 @@ namespace BuildWeek2_Be_Team7.Services
 
         public async Task<EditHospitalizationDto?> EditGetHospit(Guid hospitId)
         {
-            var hospit = await _context.Hospitalizations.FirstOrDefaultAsync(p=> p.HospitalizationId == hospitId);
-            if (hospit == null) {return null;}
+            var hospit = await _context.Hospitalizations.FirstOrDefaultAsync(p => p.HospitalizationId == hospitId);
+            if (hospit == null) { return null; }
             var edithospit = new EditHospitalizationDto()
             {
                 StartDate = hospit.StartDate,
@@ -56,7 +57,7 @@ namespace BuildWeek2_Be_Team7.Services
         public async Task<bool> EditSaveHospit(Guid hospitId, EditHospitalizationDto editHospitalizationDto)
         {
             var hospit = _context.Hospitalizations.FirstOrDefault(p => p.HospitalizationId == hospitId);
-            if (hospit == null) { return false;}
+            if (hospit == null) { return false; }
             hospit.StartDate = editHospitalizationDto.StartDate;
             hospit.EndDate = editHospitalizationDto.EndDate;
             return await SaveAsync();
@@ -74,7 +75,7 @@ namespace BuildWeek2_Be_Team7.Services
         public async Task<List<SingleHospitalizationDto>> GetAllHospitActive()
         {
             var ListHospit = new List<SingleHospitalizationDto>();
-            var data = await _context.Hospitalizations.Include(p => p.Pet).ThenInclude(p=> p.Race).Where(p => p.EndDate == null).ToListAsync();
+            var data = await _context.Hospitalizations.Include(p => p.Pet).ThenInclude(p => p.Race).Where(p => p.EndDate == null).ToListAsync();
             if (data.Count == 0) { return ListHospit; }
             ListHospit = data.Select(item => new SingleHospitalizationDto()
             {
@@ -91,6 +92,35 @@ namespace BuildWeek2_Be_Team7.Services
                 }
             }).ToList();
             return ListHospit;
+        }
+
+        public async Task<(bool, SinglePetDto?)> GetHospitalizedPetAsync(string microchip)
+        {
+            try
+            {
+                var hospitalization = await _context.Hospitalizations.Include(h => h.Pet).ThenInclude(p => p.Race).FirstOrDefaultAsync(h => h.Pet.Microchip == microchip);
+
+                if (hospitalization == null)
+                {
+                    return (false, null);
+                }
+
+                var pet = new SinglePetDto()
+                {
+                    RegistrationDate = hospitalization.Pet.RegistrationDate,
+                    Name = hospitalization.Pet.Name,
+                    Color = hospitalization.Pet.Color,
+                    Race = hospitalization.Pet.Race.Name,
+                    BirthDate = hospitalization.Pet.BirthDate,
+                    Microchip = hospitalization.Pet.Microchip
+                };
+
+                return (true, pet);
+            }
+            catch
+            {
+                return (false, null);
+            }
         }
     }
 }
