d25de2e456eba2c7b5ba428f4d84618ed334b6e0
Domy15 <domenico2016dellaventura@gmail.com>
2025-04-03 15:53:05 +0200

    minuti camabiati

--- BuildWeek2-Be-Team7/Controllers/AccountController.cs
+++ BuildWeek2-Be-Team7/Controllers/AccountController.cs
@@ -111,7 +111,7 @@ namespace BuildWeek2_Be_Team7.Controllers
             {
                 Token = tokenString,
                 //Expires = expiry,
-            });
+            }); 
         }
 
 
